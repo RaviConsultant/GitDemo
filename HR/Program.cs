0fbe50972e2ea1cfd212fd1bb12b7cc72c73d722
Ravi <ravi@gmail.com>
2022-03-23 06:48:38 +0530

    initial commit

---
+++ HR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
