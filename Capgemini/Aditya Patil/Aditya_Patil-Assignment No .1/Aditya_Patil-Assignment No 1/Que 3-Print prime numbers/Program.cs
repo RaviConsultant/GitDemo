f5817668e82b6915944c48c21ad457118841a5c9
Aditya6029 <102046063+Aditya6029@users.noreply.github.com>
2022-04-15 20:05:29 +0530

    Add files via upload

---
+++ Capgemini/Aditya Patil/Aditya_Patil-Assignment No .1/Aditya_Patil-Assignment No 1/Que 3-Print prime numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Que_3_Print_prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i = 3, count, c;
+
+            Console.WriteLine("Please enter the number of prime numbers");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 1)
+            {
+
+                Console.WriteLine("First " + num + " Prime Numbers are :\n");
+                Console.WriteLine("2");
+            }
+
+            for (count = 2; count <= num;)
+            {
+                for (c = 2; c <= i - 1; c++)
+                {
+                    if (i % c == 0)
+                        break;
+                }
+              
+                if (c == i)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+                i++;
+            }
+            Console.ReadLine();
+            }
+        }
+    }
---
+++ Capgemini/Aditya Patil/Aditya_Patil-Assignment No .1/Aditya_Patil-Assignment No 1/Question no 1-Sum of digit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_no_1_Sum_of_digit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { //Question no- 1
+            Console.WriteLine(" Enter any number:");   //Taking input from user 
+            int input = Convert.ToInt32(Console.ReadLine()); //Accepting input from user
+            int sum = 0;
+            while (input != 0)
+            {
+                sum += input % 10;  //Formula apply  
+
+                input /= 10;
+            }
+            Console.WriteLine("The sum of digits of given number is:" + sum);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
