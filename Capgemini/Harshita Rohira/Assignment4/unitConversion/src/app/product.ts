fa090bf2c58b7ba89eb6147bf01e8f2528552767
harshita-rohira <89454763+harshita-rohira@users.noreply.github.com>
2022-04-15 10:51:31 +0530

    Add files via upload

---
+++ Capgemini/Harshita Rohira/Assignment4/unitConversion/src/app/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+  id: number;
+  title: string;
+  desc: string;
+  salePrice: number;
+  current_stock: number;
+
+  constructor() {
+    this.id = 0;
+    this.title = "";
+    this.desc = "";
+    this.salePrice = 0;
+    this.current_stock = 0;
+   }
+}
