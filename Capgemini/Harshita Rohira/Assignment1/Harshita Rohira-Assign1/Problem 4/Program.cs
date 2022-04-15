c5816ff75553e3914138c12cb674e71e28d630bf
harshita-rohira <89454763+harshita-rohira@users.noreply.github.com>
2022-04-15 10:12:11 +0530

    Add files via upload

---
+++ Capgemini/Harshita Rohira/Assignment1/Harshita Rohira-Assign1/Problem 4/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Capgemini
+{
+    internal class Problem4
+    {
+        /// <summary>
+        /// To Check if given string is palindrome or not
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string palindrome(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            string s1 = new string(arr);
+            bool p = s.Equals(s1);
+            if (p == true)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Problem4 p = new Problem4();
+                //Taking a string as input to check if string is palindrome or not
+                
+                string s;
+                do
+                {
+                    Console.WriteLine("Enter a string:");
+                    s = Console.ReadLine();
+                }
+                while (string.IsNullOrEmpty(s));
+                Console.WriteLine(p.palindrome(s));
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.ToString());
+            }
+            
+}
+
+    }
+}
---
+++ Capgemini/Harshita Rohira/Assignment1/Harshita Rohira-Assign1/Problem 5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Capgemini
+{
+    internal class Problem5
+    {
+        /// <summary>
+        /// Print table of given no. till 12 
+        /// </summary>
+        /// <param name="n"></param>
+        private void printTable(int n)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(i + " * " + n + " = " + n * i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+          
+            // Taking a natural no. as input to print its table
+            int num;
+            try
+            {
+                Problem5 p = new Problem5();
+                string input;
+                do
+                {
+                    Console.WriteLine("Please enter a number (only a positive whole number):");
+                    input = Console.ReadLine();
+                }
+                while (!(int.TryParse(input, out num)) || num < 0);
+                p.printTable(num);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.ToString());
+            }
+
+        }
+
+    }
+}
