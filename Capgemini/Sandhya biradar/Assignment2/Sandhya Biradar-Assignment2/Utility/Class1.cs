458b0e6dd03c3749210754e7483836947d5b59a6
sandhyabiradar <71543363+sandhyabiradar@users.noreply.github.com>
2022-04-15 22:53:59 +0530

    Add files via upload

---
+++ Capgemini/Sandhya biradar/Assignment2/Sandhya Biradar-Assignment2/Utility/Class1.cs
@@ -0,0 +1,32 @@
+﻿
+namespace IBank.Utility
+{
+
+    public class Address
+    {
+        public string street;
+        public string area;
+        public string city;
+        public string zip;
+    }
+    public class ContactInfo
+    {
+        public string phone;
+        public string mobile;
+        public string email;
+    }
+    public class Customer
+    {
+        public string firstName;
+        public string lastName;
+        public Address custAdd;
+        public ContactInfo custContact;
+    }
+    public class Branch
+    {
+        public string name;
+        public Address branchAdd;
+        public ContactInfo branchContact;
+    }
+
+}
