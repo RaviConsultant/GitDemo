ca3a824deb5fa0f48557ab33c93db120abcb34b6
Tarunav Chandra <56769877+tarunavchandra@users.noreply.github.com>
2022-04-15 16:37:33 +0530

    Add files via upload

---
+++ Capgemini/Tarunav/Assign_1/Question_5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace question_5
+{
+    internal class Program{
+        static void Main(string[] args){
+            int i, number;
+            Console.Write("Enter a number :");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= 10; i++)
+            {
+                Console.Write("{0} X {1} = {2} \n", number, i, number * i);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
