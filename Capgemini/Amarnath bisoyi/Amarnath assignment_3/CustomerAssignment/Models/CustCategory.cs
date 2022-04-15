000218242c522fb71aba116a820a03d3d6db773f
Amarnath-glitch <102010124+Amarnath-glitch@users.noreply.github.com>
2022-04-15 16:45:59 +0530

    Add files via upload

---
+++ Capgemini/Amarnath bisoyi/Amarnath assignment_3/CustomerAssignment/Models/CustCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CustomerAssignment.Models
+{
+    public partial class CustCategory
+    {
+        public CustCategory()
+        {
+            Customer = new HashSet<Customer>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        [MinLength(3, ErrorMessage = "Category Name should be 3 to 32 Characters")]
+        [Display(Name = "CatName")]
+        public string CatName { get; set; }
+
+        public virtual ICollection<Customer> Customer { get; set; }
+    }
+}
