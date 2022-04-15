6e28b3bda1db3ce21b9de813419844b20023ecc6
Sagarjn1612 <102017219+Sagarjn1612@users.noreply.github.com>
2022-04-15 16:46:37 +0530

    Add files via upload

---
+++ Capgemini/Sagar Nehete/Assignment 1/Problem 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oue_no_2.Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y = 1, num;
+            Console.Write("enter the number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (x = 1; x <= num; x++)
+                y = y * x;
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+    }
+}
