81c5bbff5a8491885fdc5838349a653ace8c936b
codeRED-03 <68460284+codeRED-03@users.noreply.github.com>
2022-04-15 12:28:25 +0530

    Add files via upload

---
+++ Capgemini/AnkurOmer/Assignment_3/CustomerAssignment/CustomerAssignment/Models/CustomerModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerAssignment.Models
+{
+    public class CustomerModel
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        [StringLength(32, MinimumLength = 3)]
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Dob { get; set; }
+
+        public string Email { get; set; }
+
+
+        [StringLength(12)]
+        [Required]
+        public string Mobile { get; set; }
+
+        public string Category { get; set; }
+    }
+}
