c1b23d0955c10ce0dfbcadf5e08964ef8ee2173f
adarsh-08 <101059993+adarsh-08@users.noreply.github.com>
2022-04-15 12:41:08 +0530

    Add files via upload

---
+++ Capgemini/Adarsh Gupta/Assignment2/IBank/IBank.Utility/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IBank.Utility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Address
+    {
+        public string street;
+        public string area;
+        public string city;
+        public string zip;
+    }
+    public class ContactInfo
+    {
+        public string phone;
+        public string mobile;
+        public string email;
+    }
+    public class Customer
+    {
+        public string firstName;
+        public string lastName;
+        public Address custAdd;
+        public ContactInfo custContact;
+    }
+    public class Branch
+    {
+        public string name;
+        public Address branchAdd;
+        public ContactInfo branchContact;
+    }
+}
