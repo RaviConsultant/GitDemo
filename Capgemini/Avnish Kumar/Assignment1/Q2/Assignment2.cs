a6758d0b27e76e4a0e5e044cc9c7c40eb560b27f
Avnish9756 <81463454+Avnish9756@users.noreply.github.com>
2022-04-15 11:22:50 +0530

    Add files via upload

---
+++ Capgemini/Avnish Kumar/Assignment1/Q2/Assignment2.cs
@@ -0,0 +1,25 @@
+﻿
+//Program no.2   Find Factorial of a Number
+
+ 
+
+using System;
+
+namespace Capgemini
+{
+   internal class Program
+   {
+       public static void Main(string[] args)
+       {
+           Console.WriteLine("Enter the Number");
+           int input = Convert.ToInt32(Console.ReadLine());
+           int fact = 1;
+           for (int i = 1; i <= input; i++)
+           {
+               fact = fact * i;
+           }
+           Console.WriteLine("Factorial of the number is : " + fact);
+       }
+   }
+
+}
