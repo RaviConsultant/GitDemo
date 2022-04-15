47183d77ab4e4f3c7dc9be55bba787be8e0b3595
Sagarjn1612 <102017219+Sagarjn1612@users.noreply.github.com>
2022-04-15 17:33:14 +0530

    Add files via upload

---
+++ Capgemini/Sagar Nehete/Assignment 3/CustomerAssignment/Models/CustomersContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+
+namespace CustomerAssignment.Models
+{
+    public partial class CustomersContext : DbContext
+    {
+        public CustomersContext(DbContextOptions<CustomersContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CustCategory> CustCategory { get; set; }
+        public virtual DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=localhost; Database=Customers; Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustCategory>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CatName)
+                    .IsRequired()
+                    .HasColumnName("catName")
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Dob)
+                    .HasColumnName("DOB")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(128)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(32)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mobile).HasColumnType("numeric(12, 0)");
+
+                entity.HasOne(d => d.CategoryNavigation)
+                    .WithMany(p => p.Customer)
+                    .HasForeignKey(d => d.Category)
+                    .HasConstraintName("fk_Cust");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
