4b7060e1cbeca1eb50d4d21f2ee8067472a9bd42
harshita-rohira <89454763+harshita-rohira@users.noreply.github.com>
2022-04-15 10:18:59 +0530

    Add files via upload

---
+++ Capgemini/Harshita Rohira/Assignment3/CustomerAssignment/CustomerAssignment/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerAssignment.Models
+{
+    public partial class Customer
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(32)]
+        [MinLength(3, ErrorMessage = "First Name should be 3 to 32 Characters")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+       // public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? Dob { get; set; }
+        public int Mobile { get; set; }
+        public string Email { get; set; }
+        public int? Category { get; set; }
+
+        public virtual CustCategory CategoryNavigation { get; set; }
+    }
+}
