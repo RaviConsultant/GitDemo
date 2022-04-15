1bd1d06391c4b56fa63fa051ca0082caa0189486
Mohit Shikre <74311361+mohitshikre192@users.noreply.github.com>
2022-04-15 10:42:34 +0530

    Add files via upload

---
+++ Capgemini/Mohit Shikre/Assignment2/Mohit Shikre-Assign2/MohitOPP/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using IBank.Utility1;
+
+namespace IBank.Design1
+{
+
+    /// <summary>
+    /// Creating Account class's data members and methods as per problemdesign statement
+    /// </summary>
+    public class Account
+    {
+        public Account() { }
+        public int AccNo;
+        Customer Cust;
+        Branch Branch;
+        int CurrentBalance;
+        public void OpenAccount() { Console.WriteLine("Opening an account"); }
+        public void Deposit() { Console.WriteLine("Deposit money in account"); }
+        public void Close_Account() { Console.WriteLine("Closing an account"); }
+    }
+    public class SB_Account : Account
+    {
+        public void Withdraw()
+        {
+            Console.WriteLine("Withdraw money from account");
+        }
+    }
+    public class Current_Account : Account
+    {
+        public void Withdraw()
+        {
+            Console.WriteLine("Withdraw money from account");
+        }
+    }
+    public class FD_Account : Account { }
+    public class RD_Account : Account { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// example of impementing of type saving account
+            SB_Account sbaccount = new SB_Account();
+            sbaccount.OpenAccount();
+            sbaccount.Deposit();
+            sbaccount.Withdraw();
+            sbaccount.Close_Account();
+
+            // // example of impementing of type Current account
+            Current_Account cuaccount = new Current_Account();
+            cuaccount.OpenAccount();
+            cuaccount.Deposit();
+            cuaccount.Withdraw();
+            cuaccount.Close_Account();
+
+            // example of impementing of type FD account
+            FD_Account fdaccount = new FD_Account();
+            fdaccount.OpenAccount();
+            fdaccount.Deposit();
+            fdaccount.Close_Account();
+
+            //// example of impementing of type RD account
+            RD_Account rdaccount = new RD_Account();
+            rdaccount.OpenAccount();
+            rdaccount.Deposit();
+            
+            rdaccount.Close_Account();
+        }
+    }
+}
---
+++ Capgemini/Mohit Shikre/Assignment2/Mohit Shikre-Assign2/Utility/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IBank.Utility1
+{/// <summary>
+/// /creating multiple struct in library class
+/// to remove code redundancy
+/// </summary>
+    public struct Address// creating structure of Address with different attributes
+    {
+        string street;
+        string area;
+        string city;
+        string zip;
+    }
+    public struct ContactInfo // creating structure of ContactInfo with different attributes
+    {
+        string phone;
+        string mobile;
+        string Email;
+    }
+    public struct Branch// creating structure of Branch with different attributes
+    {
+        string b_name;
+        Address b_address;
+        ContactInfo b_contactInfo;
+
+    }
+    public struct Customer // creating structure of Customer with different attributes
+    {
+        string First_Name;
+        string Last_Name;
+        Address address;
+        ContactInfo contactinfo;
+    }
+}
