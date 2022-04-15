8ef125b6411dd5aa13300a4cad4fddc98d0451e0
Rutik Tupe <61056959+Rutik-byte@users.noreply.github.com>
2022-04-15 09:53:25 +0100

    Update product-list.component.ts

--- Capgemini/Rutik Tupe/Assignment 4/src/app/product-list/product-list.component.ts
+++ Capgemini/Rutik Tupe/Assignment 4/src/app/product-list/product-list.component.ts
@@ -13,39 +13,39 @@ export class ProductListComponent implements OnInit {
   constructor() {
     this.products = [
       {
-        id: 11 , 
+        id: 10 , 
         title: "Laptop",
-        desc: " Apple MacBook Pro",
-        salePrice: 112990,
-        current_stock: 4
+        descrip: " ASUS TUF",
+        salesPrice: 85000,
+        currentstock: 1
+      },
+      {
+        id: 11,
+        title: "Laptop",
+        descrip: "Dell Alienware x15 ",
+        salesPrice:250000,
+        currentstock: 3
       },
       {
         id: 12,
         title: "Mobile",
-        desc: "Samsung Galaxy S22 ultra ",
-        salePrice:60000,
-        current_stock: 4
+        descrip: "Moto Edge 20 Cyan",
+        salesPrice: 40000,
+        currentstock: 1
       },
       {
         id: 13,
-        title: "Laptop",
-        desc: "Dell XPS 13 laptop",
-        salePrice: 117000,
-        current_stock: 2
+        title: "Mobile",
+        descrip: "Google Pixel 6 Pro 5G",
+        salesPrice: 85000,
+        currentstock: 2
       },
       {
         id: 14,
         title: "Mobile",
-        desc: "Apple iPhone 13 Pro (1TB) - Gold",
-        salePrice: 169000,
-        current_stock: 2
-      },
-      {
-        id: 15,
-        title: "Laptop",
-        desc: "Alienware M15 R6 Gaming Laptop",
-        salePrice: 239989,
-        current_stock: 1
+        descrip: "Samsung Galaxy Z Flip 3 5G",
+        salesPrice: 85000,
+        currentstock: 6
       },
       
     ]
