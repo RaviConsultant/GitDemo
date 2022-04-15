7352487f194e32fa27b3e48eb8bbe657ff9ed83c
AmulyaAlamanda <102501538+AmulyaAlamanda@users.noreply.github.com>
2022-04-15 13:00:39 +0530

    Add files via upload

---
+++ Capgemini/Amulya/Amulya_ASSIGNMENT1/Amulya_ASSIGNMENT1/Program Assignment 1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Capgemini
+{
+    internal class Problem1
+    {
+
+        /// <summary>
+        /// Calculating sum of digits
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        private int digitSum(int num)
+        {
+            int sumOfDigits = 0;
+            for (; num != 0; num = num / 10)
+            {
+                sumOfDigits = sumOfDigits + num % 10;
+            }
+            return sumOfDigits;
+        }
+
+
+        static void Main(string[] args)
+        {
+            //Taking a no. as input to find sum of the digits of the given no.
+            int number;
+            try
+            {
+                Problem1 p = new Problem1();
+                string input;
+                do
+                {
+                    Console.WriteLine("Please enter a number (only a positive whole number):");
+                    input = Console.ReadLine();
+
+                }
+                while (!(int.TryParse(input, out number)) || number < 0);
+                Console.WriteLine("The sum of digits of " + input + " is " + p.digitSum(number));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: "+ex.ToString());
+            }
+        }
+
+    }
+}
+
+
+
+
+
