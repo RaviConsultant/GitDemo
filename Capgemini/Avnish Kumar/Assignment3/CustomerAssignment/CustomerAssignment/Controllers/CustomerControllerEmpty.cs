e15bdd9dfef12a4dbc05f8d42a2aa1c13c475bf3
Avnish9756 <81463454+Avnish9756@users.noreply.github.com>
2022-04-15 20:03:58 +0530

    Add files via upload

---
+++ Capgemini/Avnish Kumar/Assignment3/CustomerAssignment/CustomerAssignment/Controllers/CustomerControllerEmpty.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAssignment.Controllers
+{
+    public class CustomerControllerEmpty : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
