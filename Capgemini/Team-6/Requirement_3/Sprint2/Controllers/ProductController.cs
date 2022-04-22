5d3e23b23cddac3b2125e3e901784a195758c532
Tarunav Chandra <56769877+tarunavchandra@users.noreply.github.com>
2022-04-22 20:46:41 +0530

    Add files via upload

---
+++ Capgemini/Team-6/Requirement_3/Sprint2/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using Sprint2.Models;
+
+namespace Sprint2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            IEnumerable<Product> Product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/");
+                var responseTask = client.GetAsync("Product");
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<IList<Product>>();
+                    readJob.Wait();
+                    Product = readJob.Result;
+                }
+                else
+                {
+                    Product = Enumerable.Empty<Product>();
+                    ModelState.AddModelError(string.Empty, "Server Error occured!");
+                }
+            }
+            return View(Product);
+        }
+
+        public ActionResult ProductDetails(int id)
+        {
+            Product data = new Product();
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:5001/");
+                var consume = client.GetAsync("Product/" + id.ToString());
+                consume.Wait();
+                var test = consume.Result;
+                if (test.IsSuccessStatusCode)
+                {
+                    var display = test.Content.ReadAsAsync<Product>();
+                    //display.Wait();
+                    data = display.Result;
+                }
+              
+
+            }
+            catch (AggregateException err)
+            {
+                foreach (var errInner in err.InnerExceptions)
+                {
+                    Debug.WriteLine(errInner); //this will call ToString() on the inner execption and get you message, stacktrace and you could perhaps drill down further into the inner exception of it if necessary 
+                }
+            }
+            return View(data);
+
+
+        }
+
+    }
+}
---
+++ Capgemini/Team-6/Requirement_3/Sprint2/Models/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sprint2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+     
+
+        public string Title { get; set; }
+
+
+        public string brandName { get; set; }
+
+
+
+        public string Description { get; set; }
+
+        public double SalePrice { get; set; }
+
+        
+        public float Tax { get; set; }
+
+    
+        public uint ROM { get; set; }
+
+        public uint RAM { get; set; }
+
+     
+
+        public uint PrimaryCam { get; set; }
+
+      
+
+        public uint MainCam { get; set; }
+
+        
+
+        public int Battery { get; set; }
+
+
+        public string Processor { get; set; }
+
+
+        public int curStock { get; set; }
+
+    }
+}
