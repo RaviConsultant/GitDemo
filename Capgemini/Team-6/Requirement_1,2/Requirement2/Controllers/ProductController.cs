14f476a51814d01047a05f210bc38d48857d9296
Tarunav Chandra <56769877+tarunavchandra@users.noreply.github.com>
2022-04-22 20:43:43 +0530

    Add files via upload

---
+++ Capgemini/Team-6/Requirement_1,2/Requirement2/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Requirement.Data.Models;
+using Requirement.Services.Products;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Requirement2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private ProductService prodService = new ProductService();
+        [HttpGet]
+        public IEnumerable<Product> Index()
+        {
+            return prodService.GetProduct();
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product> Index(int id)
+        {
+            return prodService.GetProduct(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product prod)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new System.Exception("Invalid Product");
+            }
+            else
+            {
+                prodService.AddProduct(prod);
+                return prod;
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Product>> PutProduct(short id, Product prod)
+        {
+            if (id != prod.Id)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                throw new System.Exception("Invalid Product");
+            }
+            else
+            {
+                prodService.UpdateProduct(prod);
+                return prod;
+            }
+        }
+
+    }
+}
