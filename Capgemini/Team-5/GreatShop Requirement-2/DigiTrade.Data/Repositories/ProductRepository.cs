710b74ffe172ebfc0e91150e195f8790c4dc37bf
Jayantawari2000 <97623708+Jayantawari2000@users.noreply.github.com>
2022-04-22 18:18:59 +0530

    Add files via upload

---
+++ Capgemini/Team-5/GreatShop Requirement-2/DigiTrade.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,61 @@
+﻿using DigiTrade.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DigiTrade.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private DigiTradeShoppingContext _DbApi;
+
+        private IEnumerable<Products> _products;
+        public ProductRepository()
+        {
+            _DbApi = new DigiTradeShoppingContext();
+            _products = _DbApi.Products;
+        }
+        public IEnumerable<Products> GetProducts()
+        {
+            var products = from product in _DbApi.Products.Include(product => product.Brand)
+                           select new Products
+                           {
+                               ProdId = product.ProdId,
+                               ProdName = product.ProdName,
+                               Processor = product.Processor,
+                               Ram = product.Ram,
+                               Rom = product.Rom,
+                               PrimaryCam = product.PrimaryCam,
+                               FrontCam = product.FrontCam,
+                               Battery = product.Battery,
+                               Tax = product.Tax,
+                               SalesPrice = product.SalesPrice - ((int)product.SalesPrice / 10),
+                               BrandId = product.BrandId,
+                               Brand = product.Brand,
+
+
+
+                           };
+            return (IEnumerable<Products>)products;
+        }
+        public Products GetProducts(int id)
+        {
+            Products product = _products.FirstOrDefault(product => product.ProdId == id);
+            return product;
+        }
+        public Products AddProduct(Products product)
+        {
+            _DbApi.Products.Add(product);
+            _DbApi.SaveChangesAsync();
+            return product;
+        }
+        public Products UpdateProduct(Products product)
+        {
+            _DbApi.Update(product);
+            _DbApi.SaveChangesAsync();
+            return product;
+        }
+    }
+}
