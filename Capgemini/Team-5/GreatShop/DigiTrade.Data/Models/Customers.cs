be99f70c1dc377739fd2d1d880be58d421d7c6db
Jayantawari2000 <97623708+Jayantawari2000@users.noreply.github.com>
2022-04-22 18:16:18 +0530

    Add files via upload

---
+++ Capgemini/Team-5/GreatShop/DigiTrade.Data/Models/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DigiTrade.Data.Models
+{
+    public partial class Customers
+    {
+        public Customers()
+        {
+            SaleInvoices = new HashSet<SaleInvoices>();
+        }
+
+        public int CustId { get; set; }
+        public string CustName { get; set; }
+        public string Mobile { get; set; }
+        public string EmailId { get; set; }
+        public int? AddressId { get; set; }
+
+        public virtual Address Address { get; set; }
+        public virtual ICollection<SaleInvoices> SaleInvoices { get; set; }
+    }
+}
---
+++ Capgemini/Team-5/GreatShop/DigiTrade.Data/Models/SaleInvoices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace DigiTrade.Data.Models
+{
+    public partial class SaleInvoices
+    {
+        public int InvoiceNo { get; set; }
+        public DateTime? Invoicedate { get; set; }
+        public int? CustId { get; set; }
+        public int? ProdId { get; set; }
+        public int Qty { get; set; }
+        public int Rate { get; set; }
+
+        public virtual Customers Cust { get; set; }
+        public virtual Products Prod { get; set; }
+    }
+}
