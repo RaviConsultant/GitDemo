18af130bb81d978abf33781bc29acab1b7ebec7d
NamanrajSinghShaktawat <81098108+NamanrajSinghShaktawat@users.noreply.github.com>
2022-04-24 23:54:12 +0530

    Add files via upload

---
+++ Capgemini/Team-3/Requirement 3/DigiTrade.Demo/DigiTrade.Demo/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using DigiTrade.Demo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+//using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DigiTrade.Demo.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //string baseURL = "https://localhost:44355/";
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+        Product product = new Product();
+        List<Product> products = new List<Product>();
+        public ProductsController()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            products = new List<Product>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44355/api/Products"))
+                  {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    products = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+                }
+            }
+            return products;
+        }
+        [HttpGet("{id}")]
+        public async Task<Product> GetProduct(int id)
+        {
+            product = new Product();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44355/api/Products/"+id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    product = JsonConvert.DeserializeObject<Product>(apiResponse);
+                }
+            }
+
+            return product;
+        }
+
+
+    }
+}
---
+++ Capgemini/Team-3/Requirement 3/DigiTrade.Demo/DigiTrade.Demo/Models/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DigiTrade.Demo.Models
+{
+    public class Product
+    {
+        //public int Id { get; set; }
+
+        //public string ProductTitle { get; set; }
+
+        //public string ProductDescription { get; set; }
+
+        //public decimal SalePrice { get; set; }
+
+        //public int CurrentStock { get; set; }
+
+        //public string Brand { get; set; }
+
+        //public string Processor { get; set; }
+
+        //public int RAM { get; set; }
+
+        //public int ROM { get; set; }
+
+        //public int PrimaryCamera { get; set; }
+
+        //public int FrontCamera { get; set; }
+
+        //public int Battery { get; set; }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        // public long PurPrice { get; set; }
+        public long SalePrice { get; set; }
+        public long CurStock { get; set; }
+        public byte? Tax { get; set; }
+        public int? BrandId { get; set; }
+        public string Processor { get; set; }
+        public short Ram { get; set; }
+        public short Rom { get; set; }
+        public byte PrimaryCam { get; set; }
+        public byte FrontCam { get; set; }
+        public short Battery { get; set; }
+
+       // public virtual Brand Brand { get; set; }
+    }
+}
