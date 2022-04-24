d33011b1305a6ff5d786c49d6cc8b265cb018a64
prachipatidar <85191809+prachipatidar@users.noreply.github.com>
2022-04-24 23:50:26 +0530

    Add files via upload

---
+++ Capgemini/Team-1/Requirement-3/GreatShopping1/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreatShopping1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductTitle { get; set; }
+        public string Description { get; set; }
+        public long SalePrice { get; set; }
+        public long Cur_Stock { get; set; }
+        public long Tax { get; set; }
+        public int BrandId { get; set; }
+        public string Processor { get; set; }
+        public long Ram { get; set; }
+        public long Rom { get; set; }
+        public long PrimaryCamera { get; set; }
+        public long FrontCamera { get; set; }
+        public long Battery { get; set; }
+
+        //public virtual Brand Brand { get; set; }
+    }
+}
